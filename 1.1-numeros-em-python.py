0f3003afc5b1e2f9e27e614d6b4b11cf71164499
Adriel Marinho <adrielmarinho2009@gmail.com>
2026-05-13 15:49:41 -0300

    aula 2 de python

--- 1.1-numeros-em-python.py
+++ 1.1-numeros-em-python.py
@@ -63,3 +63,44 @@ print("valor: ", numero_complexo)
 print("tipo: ", type(numero_complexo))
 
 print("---------------------------------")
+
+#EXEMPLO 04 - ACESSANDO CADA PARTE DO NÚMERO
+
+# .REAL RETORNA A PARTE REAL
+print ("Parte Real: ", numero_complexo.real)
+
+# .IMAG RETORNA A PARTE IMAGINARIA 
+print("Parte Imaginaria: ", numero_complexo.imag)
+
+#APENAS PARA SEPARAR VISUAL VISUALMENTE A SAIDA DO TERMINAL
+print("\n\n")
+
+#================================================
+
+## PASSO 02 - CONVERSÃO DE DADOS
+
+#================================================
+
+## EXEMPLOS CLASSICOS
+## DADOS VINDOS DO USUARIO SÃO TEXTO(STRING), MUITAS VEZES É NECESSARIO CONVERTER ELES.
+
+print("======== Conversões ========")
+
+valor = int(3.9)
+
+print("int(3.9): ", valor)
+print("Tipos: ", type(valor))
+
+#STRING -> INT
+
+valor1 = int("10")
+print(type(valor1))
+
+valor2 = int("10")
+print('int("10"): ', valor)
+print("tipo: ", type(valor2))
+
+#INT --> FLOAT
+valor3 = float(10)
+print("float(10): ", valor3)
+print("tipo: ", type(valor3))
